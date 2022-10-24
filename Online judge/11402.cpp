b49d39b6da57936e3d09d2af58bad4d34b005923
John-Sanchez-Chilo <jsanchezchi@unsa.edu.pe>
2022-10-24 10:33:00 -0500

    pirat

---
+++ Online judge/11402.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int c,n,m,q,a,b;
+    string str;
+    for(int i=0;i<c;i++){
+        cin>>n;
+        for(int j=0;j<n;j++){
+            cin>>m>>str;
+        }
+        cin>>q;
+        for(int j=0;j<n;j++){
+            cin>>str,a,b;
+        }
+    }
+    return 0;
+}
