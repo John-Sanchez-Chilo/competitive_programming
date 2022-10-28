c5320f7519cd264851e36ec38e65e200b34d1c47
John-Sanchez-Chilo <jsanchezchi@unsa.edu.pe>
2022-10-28 16:07:48 -0500

    Judge

---
+++ Online judge/10487.cpp
@@ -0,0 +1,36 @@
+//closest sums
+#include <iostream>
+using namespace std;
+
+long long p[1000];
+int main(){
+    long long n,m,q,tmp,min_sum,n_test=1;
+    cin>>n;
+    while(true){
+        if(n==0){
+            break;
+        }
+        for(int i=0;i<n;i++){
+            cin>>p[i];
+        }
+        cin>>m;
+        cout<<"Case "<<n_test<<":\n";
+        for(int i=0;i<m;i++){
+            cin>>q;
+            min_sum=p[0]+p[1];
+            for(int i=0;i<n;i++){
+                for(int j=i+1;j<n;j++){
+                    tmp=p[i]+p[j];
+                    if(abs(min_sum-q)>abs(tmp-q)){
+                        min_sum=tmp;
+                    }
+                }
+            }
+            cout<<"Closest sum to "<<q<<" is "<<min_sum<<".\n";
+
+        }
+        n_test++;
+        cin>>n;
+    }
+    return 0;
+}
